fc21c5ef7bdb4830b456565221c99295b69cec9c
bmottag <benmotta@gmail.com>
2025-05-28 12:33:32 -0600

    Name adjustment

--- src/config/index.ts
+++ src/config/index.ts
@@ -25,8 +25,8 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Benjamin Motta",
+    specialty: "Web Developer",
     summary:
       "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
     email: "example@email.com",
