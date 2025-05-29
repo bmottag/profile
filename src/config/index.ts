0b958c7d9f2256b926979e9da07a6efc42ce246e
bmottag <benmotta@gmail.com>
2025-05-28 22:13:09 -0600

    Link adjustments

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,11 +13,11 @@ export const SITE_CONFIG: SiteConfig = {
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    //{ text: "Twitter", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://github.com/bmottag" },
+    { text: "Github", href: "https://github.com/bmottag" },
+    //{ text: "Youtube", href: "https://github.com/immois/astro-zen" },
+    //{ text: "Dribbble", href: "https://github.com/immois/astro-zen" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -29,7 +29,7 @@ export const SITE_CONTENT: SiteContent = {
     specialty: "Software Developer",
     summary:
       "Software developer with a passion for creating reliable, user-focused applications using the latest technologies and modern development practices.",
-    email: "example@email.com",
+    email: "benmotta@gmail.com",
   },
   experience: [
     {
